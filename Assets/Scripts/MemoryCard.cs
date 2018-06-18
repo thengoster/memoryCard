f86cbbdf6ea2e546d265d3aa5d561b5a180ad3c0
fluffyhusky <damonngo96@gmail.com>
2018-06-18 18:14:44 -0500

    All files for completed memoryCard game

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryCard : MonoBehaviour {
+	[SerializeField] private GameObject cardBack;
+
+	[SerializeField] private SceneController Controller; // will use public methods from SceneController later
+
+	private int _id;
+
+
+	public void OnMouseDown() {
+		if (cardBack.activeSelf && Controller.canReveal) {
+			cardBack.SetActive (false);
+			Controller.CardRevealed (this);
+		}
+	}
+
+	public void Unreveal() {
+		cardBack.SetActive(true);
+	}
+
+
+	public int id {
+		get { return _id; }
+	}
+
+	public void SetCard(int id, Sprite image) {
+		_id = id;
+		GetComponent<SpriteRenderer>().sprite = image;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButton : MonoBehaviour {
+	[SerializeField] private GameObject targetObject;
+	[SerializeField] private string targetMessage;
+	public Color highlightColor = Color.cyan;
+
+
+	public void OnMouseOver() {
+		SpriteRenderer sprite = GetComponent<SpriteRenderer> ();
+		if (sprite != null) {
+			sprite.color = highlightColor;
+		}
+	}
+
+	public void OnMouseExit() {
+		SpriteRenderer sprite = GetComponent<SpriteRenderer> ();
+		if(sprite != null) {
+			sprite.color = Color.white;
+		}
+	}
+
+	public void OnMouseDown() {
+		transform.localScale = new Vector3 (1.1f, 1.1f, 1.1f);
+	}
+
+	public void OnMouseUp() {
+		transform.localScale = Vector3.one;
+		if (targetObject != null) {
+			targetObject.SendMessage(targetMessage);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
